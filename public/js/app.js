f1c257a2c6f60f362bcfb2f39b34c96b4df58cd7
Komo <windkomo@gmail.com>
2015-02-11 14:31:57 +0100

    snap menu, layout

--- public/js/app.js
+++ public/js/app.js
@@ -8,4 +8,10 @@ angular.module('komon.services', ['komon.constants', 'ui.bootstrap', 'restangula
 angular.module('komon.controllers', ['komon.services', 'ngAnimate', 'mwl.bluebird', 'angularMoment']);
 angular.module('komon.directives', ['komon.controllers']);
 
-var app = angular.module('komon', ['komon.controllers', 'komon.services', 'komon.directives', 'ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.selection', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav', 'ngTagsInput']);
+var app = angular.module('komon', ['komon.controllers', 'komon.services', 'komon.directives', 'ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.selection', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav', 'ngTagsInput', 'snap', 'ui.grid.autoResize']).config(function(snapRemoteProvider) {
+    snapRemoteProvider.globalOptions = {
+        disable: "left",
+        minPosition: -100,
+        hyperextensible: false
+    }
+});
